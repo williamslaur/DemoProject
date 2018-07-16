62ea186106a960aee64a746edafeb5ce994dbc4c
williamslaur <36214431+williamslaur@users.noreply.github.com>
2018-07-16 12:16:25 -0500

    Add files via upload

---
+++ scripts/arrays.jsx
@@ -0,0 +1,11 @@
+var santaadmin = [
+    {groupName: "Santa", adminEmail: "Admin1@Pal.com"},
+    {groupName: "Santa", adminEmail: "Admin2@Pal.com"},
+];
+var santapart = [
+    {groupName: "Santa", partEmail: "user1@Pal.com", terms: "x", survey: "x", matchEmail: "user3@Pal.com"},
+    {groupName: "Santa", partEmail: "user2@Pal.com", terms: "x", survey: "x", matchEmail: "user5@Pal.com"},
+    {groupName: "Santa", partEmail: "user3@Pal.com", terms: "x", survey: "x", matchEmail: "user1@Pal.com"},
+    {groupName: "Santa", partEmail: "user4@Pal.com", terms: "x", survey: "x", matchEmail: "user2@Pal.com"},
+    {groupName: "Santa", partEmail: "user5@Pal.com", terms: "x", survey: "x", matchEmail: "user4@Pal.com"},
+] 
