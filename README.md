92fbf60a1c5060b213ff8ab5e727ce56b5c15f85
williamslaur <williamslaur@yahoo.com>
2018-07-16 13:03:00 -0500

    all files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # DemoProject
 Files for Demo Day
+
+Open up the About Me Section first.  This is to showcase skills with tab panels and style.  This section is also reached by links from the Secret Pal Platform in the About Me section of the footer. 
+
+The Link to the PhotosbyDaniel website is a WordPress site that is c
---
+++ scripts/arrays.js
@@ -0,0 +1,11 @@
+var santaadmin = [
+    {groupName: "Santa", adminEmail: "Admin1@Pal.com"},
+    {groupName: "Santa", adminEmail: "Admin2@Pal.com"},
+];
+var santapart = [
+    {groupName: "Santa", participantEmail: "user1@Pal.com", terms: "x", survey: "x", matchEmail: "user3@Pal.com"},
+    {groupName: "Santa", participantEmail: "user2@Pal.com", terms: "x", survey: "x", matchEmail: "user5@Pal.com"},
+    {groupName: "Santa", participantEmail: "user3@Pal.com", terms: "x", survey: "x", matchEmail: "user1@Pal.com"},
+    {groupName: "Santa", participantEmail: "user4@Pal.com", terms: "x", survey: "x", matchEmail: "user2@Pal.com"},
+    {groupName: "Santa", participantEmail: "user5@Pal.com", terms: "x", survey: "x", matchEmail: "user4@Pal.com"},
+] 
