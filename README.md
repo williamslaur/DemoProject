2b8ab98d98a28746f04a4556d7d57acffccabbad
williamslaur <williamslaur@yahoo.com>
2018-07-18 14:23:09 -0500

    all files

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ Files for Demo Day
 
 Open up the About Me Section first.  This is to showcase skills with tab panels and style.  This section is also reached by links from the Secret Pal Platform in the About Me section of the footer. 
 
-The Link to the PhotosbyDaniel website is a WordPress site that is c
+The link to the PhotosbyDaniel website is a WordPress site that has a photo gallery and blog site for travels.  It is now maintained by the owner. 
+
+The link to the Secret Pal Platform is to help with managing a secret Pal form.  There is no back end attached at the moment, but an object array has been saved to simulate the functionality if creating the tables and the data that is displayed. 
+
+Use group name Santa and Admin1@Pal.com or Admin2@Pal.com for the admin testing and Santa 
+and 
--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,66 @@
 $(document).ready(function(){
 
-//verifying input is not empty or in both fields
+// array objects to represent data in a back end
 
-function inputTest() {
-if ($(participantEmail).val==0 && $(adminEmail).val==0)
-  alert("Please enter a valid email address before clicking Submit.")
+const santaadmin = [
+  {groupName: "Santa", adminEmail: "Admin1@Pal.com"},
+  {groupName: "Santa", adminEmail: "Admin2@Pal.com"},
+];
+const santapart = [
+  {groupName: "Santa", participantEmail: "user1@Pal.com", terms: "x", survey: "x", matchEmail: "user3@Pal.com"},
+  {groupName: "Santa", participantEmail: "user2@Pal.com", terms: "x", survey: "x", matchEmail: "user5@Pal.com"},
+  {groupName: "Santa", participantEmail: "user3@Pal.com", terms: "x", survey: "x", matchEmail: "user1@Pal.com"},
+  {groupName: "Santa", participantEmail: "user4@Pal.com", terms: "x", survey: "x", matchEmail: "user2@Pal.com"},
+  {groupName: "Santa", participantEmail: "user5@Pal.com", terms: "x", survey: "x", matchEmail: "user4@Pal.com"},
+] 
 
-  if ($(participantEmail).val!==0 && $(adminEmail).val!==0)
-  alert("Please enter a valid email in only one box and click Submit.")
+//event listener on partSubmit
 
-};
+$(partSubmit).click(function(){
 
+  //verifying input is not empty and checking email is valid
 
-// testing that email has valid items in email
+    if ($(participantEmail).val=="") {
+    alert("Please enter a valid email address before clicking Submit.")
+    } else {
+      var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 
-function validateEmail(email) {
-  var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-  return emailReg.test( email );
-};
+      return emailReg.test(participantEmail)};
 
-var clicked = jQuery("button");
+    //if pass validation - test to see if groupname and participant email are in data
 
-clicked.click(function() {
-  var email = jQuery("input[type='email']").val();
-  ( !validateEmail(email) ) ? alert('Please enter a valid email address') : alert('yes') 
+    var x = $(participantEmail).val();
+    var newArray = $(groupName).val() + "part"
+   
+
+    function findUser(participantEmail) {
+      for(let i = 0; i<newArray.length; ++i) {
+        if(newArray[i].participantEmail === x) {
+          return newArray[i];
+        }
+      }
+
+    }
 });
 
+//event listener on adminSubmit
 
+  $(adminSubmit).click(function(){
+
+    //verifying input is not empty and checking email is valid
+
+    if ($(adminEmail).val=="") {
+      alert("Please enter a valid email address before clicking Submit.")
+      } else {
+        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+
+        return emailReg.test(adminEmail)
+      }
+  });
+
+
+      // test to see if group name and admin email are in data
+
+    
 
 })
