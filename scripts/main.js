dd95b5c61eae127fde5746a37c58211e056479eb
williamslaur <williamslaur@yahoo.com>
2018-07-25 16:21:22 -0500

    all

--- scripts/main.js
+++ scripts/main.js
@@ -2,17 +2,20 @@ $(document).ready(function(){
 
 // array objects to represent data in a back end
 
-var santaadmin = [
+var adminTable = [
   {groupName: "santa", adminEmail: "admin1@pal.com"},
   {groupName: "santa", adminEmail: "admin2@pal.com"},
+  {groupName: "elves", adminEmail: "admin1@elves.com"},
 ];
 
-var santapart = [
+var partTable = [
   {groupName: "santa", participantEmail: "user1@pal.com", terms: "x", survey: "x", matchEmail: "user3@pal.com"},
   {groupName: "santa", participantEmail: "user2@pal.com", terms: "x", survey: "x", matchEmail: "user5@pal.com"},
   {groupName: "santa", participantEmail: "user3@pal.com", terms: "x", survey: "x", matchEmail: "user1@pal.com"},
   {groupName: "santa", participantEmail: "user4@pal.com", terms: "x", survey: "x", matchEmail: "user2@pal.com"},
   {groupName: "santa", participantEmail: "user5@pal.com", terms: "x", survey: "x", matchEmail: "user4@pal.com"},
+  {groupName: "elves", participantEmail: "user1@elves.com", terms: "x", survey: "x", matchEmail: "user3@elves.com"},
+  {groupName: "elves", participantEmail: "user3@elves.com", terms: "x", survey: "x", matchEmail: "user1@elves.com"},
 ] 
 
 //get participants information
@@ -24,53 +27,35 @@ document.getElementById("adminSubmit").addEventListener("click", compareAdminFor
 
 function comparePartForm() { 
 
-  debugger;
   var partGrpVal = document.forms["partInputFields"]["partGrpName"].value;
   var partEmVal = document.forms["partInputFields"]["partEmail"].value;
   
   var partGrpValstr = partGrpVal.toLowerCase();
   var partEmValstr = partEmVal.toLowerCase();
 
-  const partArray = partGrpValstr + "part";
 
-  //check if the new const will be found as an array - this fails - how do I convert string to object without recreating object?
-
-  array.isarray(partArray);
-
-  //go to object and find participantEmail - 
-
+  //go to object and find participantEmail -
   
-  function checkPartEmail(pal){
-    return pal.participantEmail === (partEmValstr);
-    
-  }
-  
-
-//how do I get this to realize that myArray is the same as the Santapart array listed above when I use Santa as group name?
-
-  var found = partArray.find(checkPartEmail);
-
-
-  //convert to object to JSON string - is this needed?
-
-  var myJSON = JSON.stringify(found);
-  
-
-
-  //send to jsonstore.io = how do I get the body: to input the object "found"?
-
-  fetch('https://www.jsonstore.io/04c5db65a78544c553e1a355b6629239b8e39ec0d943a88cefb9efa8da71f1ae/users/1', {
-  headers: {
-    'Content-type': 'application/json'
-  },
-  method: 'POST',
-  body: found,
-
+  var found = partTable.filter(function(element){
+    return (element.participantEmail === partEmValstr && element.groupName === partGrpValstr);
   });
 
-  //send the user to the participants page
+console.log(found);
 
-  window.location.assign("file:///C:/Users/Laura/DemoProject/participant.html")
+  if (found.length !== 0){
+    console.log(found);
+  } else {
+    alert("User not found.  Check your Group Name and email again, or contact your administrator for help.")
+  }
+  
+      
+  var myJSON = JSON.stringify(found);  //convert to object to JSON string
+
+  localStorage.setItem("participantFound", myJSON);   //we save the object as string
+
+  console.log(myJSON);
+ 
+  window.location.assign("participant.html");   //send the user to the participants page
       
   };
 
@@ -81,63 +66,39 @@ function comparePartForm() {
 //get value of administrator's input fields and identify the object to look through
 
 function compareAdminForm() {
+
+ 
+
   var x = document.forms["adminInputFields"]["adminGrpName"].value;
   var y = document.forms["adminInputFields"]["adminEmail"].value;
 
   var xstr = x.toLowerCase();
   var ystr = y.toLowerCase();
 
-  const myArray = xstr + "admin";
-
-
 //go to object and find adminEmail
 
-function checkAdminEmail(pal){
-  return pal.adminEmail === (ystr);
-  
-}
-///how can get this to realize that myArray is the same as the santaadmin object above when I use Santa as group name?
-
-var found = myArray.find(checkAdminEmail);
-
-console.log(found);
-
-//convert to object to JSON string - is this needed?
-
-var myJSON = JSON.stringify(found);
-
-
-
-  //send to jsonstore.io = how do I get the body: to input the object "found or myJSON"?
-
-  fetch('https://www.jsonstore.io/9ca1ddbf9c352de661626070ba46dfa7cc63424575caf1c51993b5b421300552/users/1', {
-  headers: {
-    'Content-type': 'application/json'
-  },
-  method: 'POST',
-  body: { name: 'jon snow', age: 31 },
-
-  });
-
-  //send the user to the administrators page
-
-  window.location.assign("file:///C:/Users/Laura/DemoProject/adminSheet.html")
-      
-  }; 
+var found = adminTable.find(function(element){
+  return (element.adminEmail === ystr && element.groupName === xstr);
 
 });
 
-// array objects to represent data in a back end
+// display an error message if the user is not in the table
 
-var santaadmin = [
-  {groupName: "santa", adminEmail: "admin1@pal.com"},
-  {groupName: "santa", adminEmail: "admin2@pal.com"},
-];
+if (found.length !== 0){
+  console.log(found);
+} else {
+  alert("User not found. Please check your Group Name and Email again or click the Browse Admin button and begin a new group.");
+}
 
-var santapart = [
-  {groupName: "santa", participantEmail: "user1@pal.com", terms: "x", survey: "x", matchEmail: "user3@pal.com"},
-  {groupName: "santa", participantEmail: "user2@pal.com", terms: "x", survey: "x", matchEmail: "user5@pal.com"},
-  {groupName: "santa", participantEmail: "user3@pal.com", terms: "x", survey: "x", matchEmail: "user1@pal.com"},
-  {groupName: "santa", participantEmail: "user4@pal.com", terms: "x", survey: "x", matchEmail: "user2@pal.com"},
-  {groupName: "santa", participantEmail: "user5@pal.com", terms: "x", survey: "x", matchEmail: "user4@pal.com"},
-] 
+//convert object to JSON string 
+
+var myJSON = JSON.stringify(found);
+
+localStorage.setItem("adminFound", myJSON);   //we save the object as string
+
+window.location.assign("adminSheet.html");   //send the user to the administrators page
+
+};
+
+});
+
--- scripts/partSignIn.js
+++ scripts/partSignIn.js
@@ -1,11 +1,23 @@
+
+//retrieve data from json local storage
+
+var participantjsonstring = localStorage.getItem("participantFound");
+
+var jsonObject = JSON.parse(participantjsonstring); 
+
+console.log(jsonObject);
+
+//input it into the section of the screen to verify
+
+document.getElementById("groupName").innerHTML = jsonObject[0].groupName;
+
+document.getElementById("emailAddress").innerHTML = jsonObject[0].participantEmail;
+
+
+
+
+
+
 $(document).ready(function(){
 
-//retrieve data from jsonstore.io saved from main.js
-
-    const user = await fetch('https://www.jsonstore.io/04c5db65a78544c553e1a355b6629239b8e39ec0d943a88cefb9efa8da71f1ae/users/1')
-    .then(function(response) {
-      return response.json();
-    })
-
-
-})
+});
